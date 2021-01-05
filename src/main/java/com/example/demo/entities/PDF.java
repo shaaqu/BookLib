35fd163579fa00067137b5d1216675faf2353afb
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-05 18:10:44 +0100

    PDF entity

--- src/main/java/com/example/demo/entities/PDF.java
+++ src/main/java/com/example/demo/entities/PDF.java
@@ -1,4 +1,24 @@
 package com.example.demo.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PDF")
 public class PDF {
+
+    @Id
+    @Column(name = "bookId")
+    private String bookId;
+
+    @Column
+    private boolean isAvailable;
+
+    @Column
+    private String acsTokenLink;
+
+    public PDF() {
+    }
 }
