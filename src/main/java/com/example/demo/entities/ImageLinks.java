ff8921a2f0fb553b6a28a4827b176890a50820cf
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-05 18:25:09 +0100

    ImageLinks entity

--- src/main/java/com/example/demo/entities/ImageLinks.java
+++ src/main/java/com/example/demo/entities/ImageLinks.java
@@ -1,9 +1,24 @@
 package com.example.demo.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "ImageLinks")
 public class ImageLinks {
+
+    @Id
+    @Column(name = "bookId")
+    private String bookId;
+
+    @Column(name = "smallThumbnail")
+    private String smallThumbnail;
+
+    @Column(name = "thumbnail")
+    private String thumbnail;
+
+    public ImageLinks() {
+    }
 }
