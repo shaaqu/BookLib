cd02a50f2202c20a8f4706e12ffe63a945e37e4f
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-05 18:20:21 +0100

    SaleInfo entity

--- src/main/java/com/example/demo/entities/SaleInfo.java
+++ src/main/java/com/example/demo/entities/SaleInfo.java
@@ -1,4 +1,24 @@
 package com.example.demo.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SaleInfo")
 public class SaleInfo {
+
+    @Id
+    @Column(name = "bookId")
+    private String bookId;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "saleAbility")
+    private String saleAbility;
+
+    public SaleInfo() {
+    }
 }
