cda3beaac2ec358026946004afc3060b19b60be6
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-06 00:00:01 +0100

    before each in BookItemDeserializerTest

--- src/test/java/com/example/demo/BookItemDeserializerTest.java
+++ src/test/java/com/example/demo/BookItemDeserializerTest.java
@@ -1,16 +1,26 @@
 package com.example.demo;
 
+import com.example.demo.entities.Book;
+import com.example.demo.services.BookJsonModule;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookItemDeserializerTest {
 
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void init() {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new BookJsonModule());
+    }
+
     @Test
-    void bookDeserialize(){
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new BookJsonModul())
+    void bookDeserializeWithoutAssertions(){
+        Book book = objectMapper.readValue()
     }
 
 }
