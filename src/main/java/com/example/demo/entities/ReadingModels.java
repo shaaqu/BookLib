963266cf44df0650c9b571369d5b2ed05672a2bb
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-05 18:27:21 +0100

    ReadingModels entity

--- src/main/java/com/example/demo/entities/ReadingModels.java
+++ src/main/java/com/example/demo/entities/ReadingModels.java
@@ -1,9 +1,24 @@
 package com.example.demo.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "readingModels")
 public class ReadingModels {
+
+    @Id
+    @Column(name = "bookId")
+    private String bookId;
+
+    @Column(name = "text")
+    private boolean text;
+
+    @Column(name = "image")
+    private boolean image;
+
+    public ReadingModels() {
+    }
 }
