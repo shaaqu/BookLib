f8eea4c937702e88047201c386807ff952275f64
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-05 18:33:41 +0100

    IndustryIdentifiers entity

--- src/main/java/com/example/demo/entities/IndustryIdentifiers.java
+++ src/main/java/com/example/demo/entities/IndustryIdentifiers.java
@@ -1,9 +1,24 @@
 package com.example.demo.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "industryIdentifiers")
 public class IndustryIdentifiers {
+
+    @Id
+    @Column(name = "bookId")
+    private String bookId;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "identifier")
+    private String identifier;
+
+    public IndustryIdentifiers() {
+    }
 }
