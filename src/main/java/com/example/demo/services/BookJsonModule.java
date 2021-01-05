3c48609f4c49324309f958de3eb52e9d46c57a01
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-05 23:55:20 +0100

    BookJsonModule.java

---
+++ src/main/java/com/example/demo/services/BookJsonModule.java
@@ -0,0 +1,17 @@
+package com.example.demo.services;
+
+import com.example.demo.BookItemDeserializer;
+import com.example.demo.entities.Book;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class BookJsonModule extends SimpleModule {
+    public BookJsonModule() {
+        this.addDeserializer(Book.class, new BookItemDeserializer());
+    }
+}
--- src/test/java/com/example/demo/BookItemDeserializerTest.java
+++ src/test/java/com/example/demo/BookItemDeserializerTest.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,7 +9,8 @@ class BookItemDeserializerTest {
 
     @Test
     void bookDeserialize(){
-
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new BookJsonModul())
     }
 
 }
