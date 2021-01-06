532ef4e732563311f5368c24bb0e9453b09aad1e
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-06 18:50:00 +0100

    EPubRepository

--- src/main/java/com/example/demo/repositories/EPubRepository.java
+++ src/main/java/com/example/demo/repositories/EPubRepository.java
@@ -1,4 +1,11 @@
 package com.example.demo.repositories;
 
-public interface EPubRepository {
+import com.example.demo.entities.EPub;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface EPubRepository extends CrudRepository<EPub, String>, PagingAndSortingRepository<EPub, String> {
+
+    EPub save(EPub ePub);
+
 }
