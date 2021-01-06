c10e13b44676ade77f80f56557d088c2c4ee959a
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-06 18:56:52 +0100

    PDFRepository

--- src/main/java/com/example/demo/repositories/PDFRepository.java
+++ src/main/java/com/example/demo/repositories/PDFRepository.java
@@ -1,4 +1,11 @@
 package com.example.demo.repositories;
 
-public interface PDFRepository {
+import com.example.demo.entities.PDF;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface PDFRepository extends CrudRepository<PDF, String>, PagingAndSortingRepository<PDF, String> {
+
+    PDF save(PDF pdf);
+
 }
