3fc88dc4fb07f974d6c7890e6420abe72a008868
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-06 22:05:27 +0100

    BookItemDeserializer

--- src/main/java/com/example/demo/BookItemDeserializer.java
+++ src/main/java/com/example/demo/BookItemDeserializer.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.demo.JSONNodes.Paths.*;
@@ -59,11 +60,11 @@ public class BookItemDeserializer extends JsonDeserializer {
         return VolumeInfo.builder()
                 .bookId(bookId)
                 .title(jsonNode.get("title").asText(null))
-                .authors(readAuthor(jsonNodes.getPath(AUTHORS)))
+                .authors(readAuthor(jsonNode))
                 .publisher(jsonNode.get("publisher").asText(null))
                 .publishedDate(jsonNode.get("publishedDate").asLong(0))
                 .description(jsonNode.get("description").asText(null))
-                .industryIdentifiers(readIndustryIdentifier(jsonNodes.getPath(INDUSTRY_IDENTIFIER)))
+                .industryIdentifiers(readIndustryIdentifier(jsonNode))
                 .readingMode(readReadingMode(jsonNodes.getPath(READING_MODES)))
                 .pageCount(jsonNode.get("pageCount").asInt(0))
                 .printType(jsonNode.get("printType").asText(null))
@@ -107,22 +108,54 @@ public class BookItemDeserializer extends JsonDeserializer {
     }
 
     public List<Author> readAuthor(JsonNode jsonNode){
-        return null;
+        List<Author> authors = new ArrayList<>();
+
+        jsonNode.findValuesAsText("authors").forEach(
+                (v) -> authors.add(Author.builder().name(v).build())
+        );
+
+        return authors;
     }
     public EPub readEPub(JsonNode jsonNode){
-        return null;
+        return EPub.builder()
+                .bookId(bookId)
+                .isAvailable(jsonNode.get("isAvailable").asBoolean(false))
+                .acsTokenLink(jsonNode.get("acsTokenLink").asText(null))
+                .build();
+
     }
     public ImageLinks readImageLinks(JsonNode jsonNode){
-        return null;
+        return ImageLinks.builder()
+                .smallThumbnail(jsonNode.get("smallThumbnail").asText(null))
+                .thumbnail(jsonNode.get("thumbnail").asText(null))
+                .build();
     }
     public List<IndustryIdentifier> readIndustryIdentifier(JsonNode jsonNode){
-        return null;
+        List<IndustryIdentifier> industryIdentifiers = new ArrayList<>();
+
+        jsonNode.findValuesAsText("industryIdentifiers").forEach(
+                (v) -> industryIdentifiers.add(IndustryIdentifier.builder()
+                        .bookId(bookId)
+                        .type(jsonNode.get("type").asText(null))
+                        .identifier(jsonNode.get("identifier").asText(null))
+                        .build())
+        );
+
+        return industryIdentifiers;
     }
     public com.example.demo.entities.PDF readPDF(JsonNode jsonNode){
-        return null;
+        return com.example.demo.entities.PDF.builder()
+                .bookId(bookId)
+                .isAvailable(jsonNode.get("isAvailable").asBoolean(false))
+                .acsTokenLink(jsonNode.get("acsTokenLink").asText(null))
+                .build();
     }
     public ReadingMode readReadingMode(JsonNode jsonNode){
-        return null;
+        return ReadingMode.builder()
+                .bookId(bookId)
+                .image(jsonNode.get("image").asBoolean(false))
+                .text(jsonNode.get("text").asBoolean(false))
+                .build();
     }
 
 }
