2e856c98f7af3f86b5b3fc6398e35801f4c600d9
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-06 18:50:55 +0100

    ImageLinksRepository

--- src/main/java/com/example/demo/repositories/ImageLinksRepository.java
+++ src/main/java/com/example/demo/repositories/ImageLinksRepository.java
@@ -1,4 +1,11 @@
 package com.example.demo.repositories;
 
-public interface ImageLinksRepository {
+import com.example.demo.entities.ImageLinks;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ImageLinksRepository extends CrudRepository<ImageLinks, String>, PagingAndSortingRepository<ImageLinks, String> {
+
+    ImageLinks save(ImageLinks imageLinks);
+
 }
