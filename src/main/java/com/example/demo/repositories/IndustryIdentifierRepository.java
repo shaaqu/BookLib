84c833ef934cf1994a24f5c81670aae827d8c9ef
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-06 18:51:49 +0100

    IndustryIdentifierRepository

--- src/main/java/com/example/demo/repositories/IndustryIdentifierRepository.java
+++ src/main/java/com/example/demo/repositories/IndustryIdentifierRepository.java
@@ -1,4 +1,12 @@
 package com.example.demo.repositories;
 
-public interface IndustryIdentifierRepository {
+import com.example.demo.entities.ImageLinks;
+import com.example.demo.entities.IndustryIdentifier;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface IndustryIdentifierRepository extends CrudRepository<IndustryIdentifier, String>, PagingAndSortingRepository<IndustryIdentifier, String> {
+
+    IndustryIdentifier save(IndustryIdentifier industryIdentifier);
+
 }
