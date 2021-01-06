51f0de1245617038a474ddfeee65acce7fcb312f
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-06 18:55:19 +0100

    SearchInfoRepository

--- src/main/java/com/example/demo/repositories/SearchInfoRepository.java
+++ src/main/java/com/example/demo/repositories/SearchInfoRepository.java
@@ -1,4 +1,11 @@
 package com.example.demo.repositories;
 
-public interface SearchInfoRepository {
+import com.example.demo.entities.SearchInfo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface SearchInfoRepository extends CrudRepository<SearchInfo, String>, PagingAndSortingRepository<SearchInfo, String> {
+
+    SearchInfo save(SearchInfo searchInfo);
+
 }
