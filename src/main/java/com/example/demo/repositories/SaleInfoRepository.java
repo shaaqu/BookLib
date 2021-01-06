c98b62347b5586f725a3e2d6366a3049025fa685
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-06 18:54:32 +0100

    SaleInfoRepository

--- src/main/java/com/example/demo/repositories/SaleInfoRepository.java
+++ src/main/java/com/example/demo/repositories/SaleInfoRepository.java
@@ -1,4 +1,11 @@
 package com.example.demo.repositories;
 
-public interface SaleInfoRepository {
+import com.example.demo.entities.SaleInfo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface SaleInfoRepository extends CrudRepository<SaleInfo, String>, PagingAndSortingRepository<SaleInfo, String> {
+
+    SaleInfo save(SaleInfo saleInfo);
+
 }
