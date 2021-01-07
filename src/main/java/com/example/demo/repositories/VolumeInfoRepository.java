28f0fdff505638bd8e980d4a07ed2af919e7cb78
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-07 17:38:04 +0100

    rating query

--- src/main/java/com/example/demo/repositories/VolumeInfoRepository.java
+++ src/main/java/com/example/demo/repositories/VolumeInfoRepository.java
@@ -2,11 +2,18 @@ package com.example.demo.repositories;
 
 import com.example.demo.entities.IndustryIdentifier;
 import com.example.demo.entities.VolumeInfo;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
 
 public interface VolumeInfoRepository extends CrudRepository<VolumeInfo, String>, PagingAndSortingRepository<VolumeInfo, String> {
 
     VolumeInfo save(VolumeInfo volumeInfo);
     VolumeInfo getVolumeInfoByIndustryIdentifiers(IndustryIdentifier industryIdentifier);
+
+    @Query("select * from VolumeInfo where averageRating is not null ")
+    List<VolumeInfo> findWithRating();
 }
