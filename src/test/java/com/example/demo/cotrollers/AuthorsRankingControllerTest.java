7199bac1bff08f02c103c0f35af5161dbec0c04b
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-13 00:07:02 +0100

    AuthorsRankingControllerTest passed

--- src/test/java/com/example/demo/cotrollers/AuthorsRankingControllerTest.java
+++ src/test/java/com/example/demo/cotrollers/AuthorsRankingControllerTest.java
@@ -1,4 +1,51 @@
 package com.example.demo.cotrollers;
 
+import com.example.demo.ItemRequestBuilder;
+import com.example.demo.ItemRequestBuilderFactory;
+import com.example.demo.controllers.AuthorsRankingController;
+import com.example.demo.services.AuthorsRankingService;
+import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static com.example.demo.ItemRequestBuilderFactory.Items.AUTHORS_RANKING_ITEM;
+import static com.example.demo.WebTestConfig.exceptionResolver;
+import static com.example.demo.WebTestConfig.fixedLocaleResolver;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class AuthorsRankingControllerTest {
+
+    private ItemRequestBuilder itemRequestBuilder;
+    private AuthorsRankingService service;
+    private ItemRequestBuilderFactory factory;
+
+    @BeforeEach
+    void configureSystemUnderTest() throws JSONException {
+        service = mock(AuthorsRankingService.class);
+        AuthorsRankingController accountController = new AuthorsRankingController(service);
+        MockMvc mockMvc = MockMvcBuilders
+                .standaloneSetup(accountController)
+                .setHandlerExceptionResolvers(exceptionResolver())
+                .setLocaleResolver(fixedLocaleResolver())
+                .build();
+        factory = new ItemRequestBuilderFactory(mockMvc);
+        itemRequestBuilder = factory.createRequestBuilder(AUTHORS_RANKING_ITEM);
+
+    }
+
+    @Test
+    public void getItemTest() throws Exception {
+        itemRequestBuilder.getItem().andExpect(status().isOk())
+                .andExpect(
+                        content().contentType(MediaType.APPLICATION_JSON)
+                );
+    }
 }
