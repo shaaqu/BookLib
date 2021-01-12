1c606ad951dfc91139ae2750cae47d89315c6e36
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-12 23:55:39 +0100

    CategoriesItemBuilder

--- src/test/java/com/example/demo/CategoriesItemBuilder.java
+++ src/test/java/com/example/demo/CategoriesItemBuilder.java
@@ -1,4 +1,20 @@
 package com.example.demo;
 
-public class CategoriesItemBuilder {
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+public class CategoriesItemBuilder implements ItemRequestBuilder{
+
+    private MockMvc mockMvc;
+
+    public CategoriesItemBuilder(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Override
+    public ResultActions getItem() throws Exception {
+        return mockMvc.perform(get("/categories/category"));
+    }
 }
