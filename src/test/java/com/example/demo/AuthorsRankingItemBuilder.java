a69e355227f1433630766044566e8c0f51e2567f
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-12 23:53:02 +0100

    AuthorsRankingItemBuilder

--- src/test/java/com/example/demo/AuthorsRankingItemBuilder.java
+++ src/test/java/com/example/demo/AuthorsRankingItemBuilder.java
@@ -1,4 +1,20 @@
 package com.example.demo;
 
-public class AuthorsRankingItemBuilder {
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+public class AuthorsRankingItemBuilder implements ItemRequestBuilder{
+
+    private MockMvc mockMvc;
+
+    public AuthorsRankingItemBuilder(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Override
+    public ResultActions getItem() throws Exception {
+        return mockMvc.perform(get("/ranking"));
+    }
 }
