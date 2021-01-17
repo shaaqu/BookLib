a8bb2242099a09951efcd7bf75f322d103d65c65
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-17 23:05:58 +0100

    deleted RatingComparator.java

--- src/main/java/com/example/demo/services/AuthorsRanksServiceImpl.java
+++ src/main/java/com/example/demo/services/AuthorsRanksServiceImpl.java
@@ -1,15 +1,12 @@
 package com.example.demo.services;
 
 import com.example.demo.Rating;
-import com.example.demo.RatingComparator;
-import com.example.demo.entities.Author;
 import com.example.demo.repositories.AuthorRepository;
 import com.example.demo.repositories.VolumeInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class AuthorsRanksServiceImpl implements AuthorsRankingService{
