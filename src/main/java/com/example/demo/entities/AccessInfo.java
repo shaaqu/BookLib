9dfc7af7764e6fea9d61b47eeacf8d2a42f0698a
shaaqu <pawel.lukaszewicz99@gmail.com>
2021-01-17 04:07:32 +0100

    saving alone

--- src/main/java/com/example/demo/entities/AccessInfo.java
+++ src/main/java/com/example/demo/entities/AccessInfo.java
@@ -1,8 +1,6 @@
 package com.example.demo.entities;
 
 import lombok.Builder;
-import org.hibernate.annotations.NotFound;
-import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
 import java.util.List;
@@ -31,7 +29,6 @@ public class AccessInfo {
     private String textToSpeechPermission;
 
     @OneToOne(mappedBy = "accessInfo", cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
-    @NotFound(action = NotFoundAction.IGNORE)
     @PrimaryKeyJoinColumn
     private EPub ePub;
 
